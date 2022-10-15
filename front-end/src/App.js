6c11058b39a067a2d2bd42bc7774c2226e42e5c2
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-15 16:52:57 -0500

    Create Theme

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero'
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material/styles';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#5FB4A2",
+        main: '#203A4C',
+        dark: '#33323D'
+        // contrastText: will be calculated to contrast with palette.primary.main
+      },
+      secondary: {
+        light: '#FAFAFA',
+        main: '#EAEAEB',
+      },
+      error: {
+        main: '#F43030'
+      }
+    }
+  });
   return (
-    <div className="App">
-      <NavBar />
-      <Hero />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <NavBar />
+        <Hero />
+      </div>
+    </ThemeProvider>
   );
 }
 
