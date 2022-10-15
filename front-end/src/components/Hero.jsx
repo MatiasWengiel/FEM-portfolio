5d9d7b464d9f4212de3cd413cd69a96573879d5e
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-15 16:53:25 -0500

    Remove duplicate textAlign

--- front-end/src/components/Hero.jsx
+++ front-end/src/components/Hero.jsx
@@ -30,7 +30,6 @@ export default function Hero(props) {
           sx={{
             fontSize: { lg: "2.5em", md: "1.75em", sm: "1.2em", xs: "1.5em" },
             fontWeight: "bold",
-            textAlign: "left",
             marginBottom: { sm: "30px", xs: "20px" },
             textAlign: "center",
           }}
