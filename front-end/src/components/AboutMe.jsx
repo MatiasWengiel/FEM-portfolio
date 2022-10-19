91d2a92e793a151bae6604e2f0d82a122478656b
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 11:12:57 -0500

    move main and image container styles, as well as image positioning and text responsive font styles to separate constants for improved code readability. Make small change to text within Typography to be consistant with resume summary

--- front-end/src/components/AboutMe.jsx
+++ front-end/src/components/AboutMe.jsx
@@ -3,60 +3,55 @@ import { Container, Typography } from "@mui/material";
 import myPicture from "../images/TestPicture.jpg";
 
 export default function AboutMe(props) {
+  const mainContainerStyles = {
+    display: "flex",
+    flexFlow: "row wrap",
+    justifyContent: "space-between",
+    mt: { xs: "96px", sm: "150px" },
+  };
+  const imageContainerStyles = {
+    m: 0,
+    width: { xs: 1, sm: 1 / 3, md: 1 / 2 },
+    height: { xs: 300, sm: 600 },
+  };
+
+  const imagePositioning = {
+    xs: "top -65px right 0",
+    sm: "top -75px left -30px",
+    md: "top -75px right 0",
+  };
+
+  const textResponsiveFont = {
+    lg: "1.75em",
+    md: "1.55em",
+    sm: "1.5em",
+    xs: "1.25em",
+  };
+
   return (
     <>
-      <Container
-        id="AboutMe"
-        sx={{
-          display: "flex",
-          flexFlow: "row wrap",
-          justifyContent: "space-between",
-          mt: { xs: "96px", sm: "150px" },
-        }}
-      >
-        <Container
-          disableGutters
-          sx={{
-            m: 0,
-            width: { xs: 1, sm: 1 / 3, md: 1 / 2 },
-            height: { xs: 300, sm: 600 },
-          }}
-        >
-          <Image
-            src={myPicture}
-            sx={{
-              objectPosition: {
-                xs: "top -65px right 0",
-                sm: "top -75px left -30px",
-                md: "top -75px right 0",
-              },
-            }}
-          />
+      <Container id="AboutMe" sx={mainContainerStyles}>
+        <Container disableGutters sx={imageContainerStyles}>
+          <Image src={myPicture} sx={{ objectPosition: imagePositioning }} />
         </Container>
         <Container sx={{ m: 0, width: { xs: 1, sm: 2 / 3, md: 1 / 2 } }}>
           <Typography
             variant="h1"
-            mb={3}
-            sx={{ fontSize: { xs: "4rem", sm: "3rem", lg: "4rem" } }}
+            sx={{ mb: 3, fontSize: { xs: "4rem", sm: "3rem", lg: "4rem" } }}
           >
             About Me
           </Typography>
           <Typography
             variant="body1"
             sx={{
-              fontSize: {
-                lg: "1.75em",
-                md: "1.55em",
-                sm: "1.5em",
-                xs: "1.25em",
-              },
+              fontSize: textResponsiveFont,
             }}
           >
             Enthusiastic Full-Stack Web Developer, passionate about writing code
             that is reusable, scalable and testable. Transitioning from a career
             in post-secondary education, with experience in both working within
             and leading multidisciplinary teams, breaking down and communicating
-            complex technical information to learners, and creating thorough
+            complex technical information, and creating thorough, detailed
             documentation.
           </Typography>
         </Container>
