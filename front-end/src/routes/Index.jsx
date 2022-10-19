5b6fd8d2aadcccd38b6a58b5876907170144dcca
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 11:39:06 -0500

    Create Index page with Hero and AboutMe components

---
+++ front-end/src/routes/Index.jsx
@@ -0,0 +1,11 @@
+import AboutMe from "../components/AboutMe";
+import Hero from "../components/Hero";
+
+export default function Index(props) {
+  return (
+    <>
+      <Hero />
+      <AboutMe />
+    </>
+  );
+}
