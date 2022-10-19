a0ca3a9515a96bb40c30988715ff1e06603fb3c6
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 14:30:38 -0500

    Add PasswordKeepR project

--- front-end/src/routes/Projects.jsx
+++ front-end/src/routes/Projects.jsx
@@ -9,8 +9,17 @@ export default function Projects(props) {
         title="Gardnr"
         pictureURL="https://raw.githubusercontent.com/MatiasWengiel/HomeGardnr--Caitlin-Croteau-Raiza-DeGuzman-Matias-Wengiel/task/finish-README/docs/Gardnr-viewunwateredplants.png"
         taglineText="Grow a beautiful, bountiful garden with Gardnr!"
-        descriptionText="This web app was created using node.js and express on the back end, and React, React-Bootstrap and SASS on the front end. Developed by Matias Wengiel, Caitlin Croteau and Raiza De Guzman as a final project for Lighthouse Lab's Full Stack Web Developer Program"
+        descriptionText="This web app was created using React, React-Bootstrap and SASS on the front end, and node.js and express on the back end. Developed by Matias Wengiel, Caitlin Croteau and Raiza De Guzman as a final project for Lighthouse Lab's Full Stack Web Developer Program"
         githubLink="https://github.com/MatiasWengiel/HomeGardnr--Caitlin-Croteau-Raiza-DeGuzman-Matias-Wengiel"
+      />
+      <Project
+        key={2}
+        id={2}
+        title="PasswordKeepR"
+        pictureURL="https://github.com/MatiasWengiel/MidtermProject-PasswordKeepR-Matias-Brian-Moe/blob/master/project_organization/PWKeepR-main-page.png?raw=true"
+        taglineText="PasswordKeepR allows for the easy and secure sharing of passwords across an organization."
+        descriptionText="This website was created using EJS, HTML and SASS on the front end, and node.js and express on the back end. Developed by Matias Wengiel, Mohammed Mahdi and Brian Nguyen as a midterm project for Lighthouse Lab's Full Stack Web Developer Program"
+        githubLink="https://github.com/MatiasWengiel/MidtermProject-PasswordKeepR-Matias-Brian-Moe"
       />
     </>
   );
