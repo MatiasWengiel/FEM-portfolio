8e22258277bea7d0b01f882dd0b72bf8c2344dc7
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 11:40:00 -0500

    Add dummy Projects page. TODO: Create Projects page

---
+++ front-end/src/routes/Projects.jsx
@@ -0,0 +1,11 @@
+import AboutMe from "../components/AboutMe";
+import Hero from "../components/Hero";
+
+export default function Projects(props) {
+  return (
+    <>
+      <AboutMe />
+      <Hero />{" "}
+    </>
+  );
+}
