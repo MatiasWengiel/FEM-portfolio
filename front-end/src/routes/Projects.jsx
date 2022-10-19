f839d20a89973bbf5bedd03419221755170cf49e
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 14:18:59 -0500

    Add github link to props. Remove duplicate project

--- front-end/src/routes/Projects.jsx
+++ front-end/src/routes/Projects.jsx
@@ -10,14 +10,7 @@ export default function Projects(props) {
         pictureURL="https://raw.githubusercontent.com/MatiasWengiel/HomeGardnr--Caitlin-Croteau-Raiza-DeGuzman-Matias-Wengiel/task/finish-README/docs/Gardnr-viewunwateredplants.png"
         taglineText="Grow a beautiful, bountiful garden with Gardnr!"
         descriptionText="This web app was created using node.js and express on the back end, and React, React-Bootstrap and SASS on the front end. Developed by Matias Wengiel, Caitlin Croteau and Raiza De Guzman as a final project for Lighthouse Lab's Full Stack Web Developer Program"
-      />
-      <Project
-        key={2}
-        id={2}
-        title="Gardnr"
-        pictureURL="https://raw.githubusercontent.com/MatiasWengiel/HomeGardnr--Caitlin-Croteau-Raiza-DeGuzman-Matias-Wengiel/task/finish-README/docs/Gardnr-viewunwateredplants.png"
-        taglineText="Grow a beautiful, bountiful garden with Gardnr!"
-        descriptionText="This web app was created using node.js and express on the back end, and React, React-Bootstrap and SASS on the front end. Developed by Matias Wengiel, Caitlin Croteau and Raiza De Guzman as a final project for Lighthouse Lab's Full Stack Web Developer Program"
+        githubLink="https://github.com/MatiasWengiel/HomeGardnr--Caitlin-Croteau-Raiza-DeGuzman-Matias-Wengiel"
       />
     </>
   );
