16c6fcd0fa9e5b3bfc21904a30dd860f3d67c5d3
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 10:57:42 -0500

    Extract AppBar styles to a separate constant for improved code readability. Add Projects button to NavBar that links to /projects. TODO: Create projects page

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,18 +1,17 @@
 import { AppBar, Button } from "@mui/material";
 
 export default function NavBar(props) {
+  const appBarStyleOverrides = {
+    height: 80,
+    p: 5,
+    display: "flex",
+    flexFlow: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    background: "white",
+  };
   return (
-    <AppBar
-      sx={{
-        height: 80,
-        p: 5,
-        display: "flex",
-        flexFlow: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        background: "white",
-      }}
-    >
+    <AppBar sx={appBarStyleOverrides}>
       <div>
         <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16">
           <path fill="none" stroke="#33323D" d="M9 0L1 8l8 8" />
@@ -22,8 +21,9 @@ export default function NavBar(props) {
         </svg>
       </div>
       <div>
-        <Button>Home</Button>
+        <Button href="/">Home</Button>
         <Button href="https://github.com/MatiasWengiel">Github</Button>
+        <Button href="/projects">Projects</Button>
         <Button>Contact Me</Button>
       </div>
     </AppBar>
