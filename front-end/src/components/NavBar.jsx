ae7dcfeac6208f5782a5d154d2fa4829121ad389
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-19 16:23:05 -0500

    WIP - Modify NavBar so that it can be responsive in mobile

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,15 +1,16 @@
-import { AppBar, Button } from "@mui/material";
+import { AppBar, Button, Box, IconButton } from "@mui/material";
 
 export default function NavBar(props) {
   const appBarStyles = {
-    height: 80,
+    minHeight: 80,
     p: 5,
     display: "flex",
     flexFlow: "row",
-    justifyContent: "space-between",
+    justifyContent: { xs: "center", sm: "space-between" },
     alignItems: "center",
     background: "white",
   };
+
   return (
     <AppBar sx={appBarStyles}>
       <div>
@@ -20,14 +21,14 @@ export default function NavBar(props) {
           <path fill="none" stroke="#33323D" d="M1 0l8 8-8 8" />
         </svg>
       </div>
-      <div>
+      <Box sx={{ display: "flex", flexFlow: { xs: "column", sm: "row" } }}>
         <Button href="/">Home</Button>
         <Button href="https://github.com/MatiasWengiel" target="_blank">
           Github
         </Button>
         <Button href="/projects">Projects</Button>
         <Button>Contact Me</Button>
-      </div>
+      </Box>
     </AppBar>
   );
 }
