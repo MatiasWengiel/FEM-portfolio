e086aef52865c575bd53439eae474b1e1a08c0c7
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-20 14:56:58 -0500

    Add Footer component

--- front-end/src/routes/Root.jsx
+++ front-end/src/routes/Root.jsx
@@ -1,11 +1,14 @@
+import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 export default function Root(props) {
   return (
-    <>
+    <Box sx={{ display: "flex", flexFlow: "column" }}>
       <NavBar />
       <Outlet />
-    </>
+      <Footer />
+    </Box>
   );
 }
